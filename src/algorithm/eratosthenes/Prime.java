f039393a4c16ed90aead8595ed9ab8f0d7d2082e
yusok7 <diddbtjr7@naver.com>
2021-11-16 11:21:51 +0900

    에라토스테네스의 체

---
+++ src/algorithm/eratosthenes/Prime.java
@@ -0,0 +1,28 @@
+package algorithm.eratosthenes;
+
+public class Prime {
+
+    // 120까지의 소수 구하기
+    public static boolean[] prime = new boolean[121];
+
+    public static void main(String[] args) {
+
+        // 구하고자 하는 숫자 범위
+        int n = 120;
+
+        // 0과 1은 소수가 아니므로 제외
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i*i <= n; i++) {
+            if (!prime[i]) {
+                for (int j = i*i; j <= n; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i <= n; i++) {
+            if (!prime[i]) System.out.println(i);
+        }
+    }
+}
