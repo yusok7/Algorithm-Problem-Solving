b88d36e60917555ade0f302f256e4f6e48bba3a1
yusok7 <diddbtjr7@naver.com>
2021-11-21 16:12:21 +0900

    Queue 자료구조

--- src/programmers/leveltwo/Printer.java
+++ src/programmers/leveltwo/Printer.java
@@ -1,59 +1,46 @@
 package programmers.leveltwo;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Printer {
-	public static void main(String[] args) {
-		int[] priorities = {2, 1, 3, 2};
-		int location = 2;
-		int solution = solution(priorities, location);
-		System.out.println("solution = " + solution);
-	}
 
-	public static int solution(int[] priorities, int location) {
+    public Queue<Boolean> locationQueue = new LinkedList<>();
+    public Queue<Integer> priorityQueue = new LinkedList<>();
 
-		int length = priorities.length;
-		int count = 1;
-		int answer = 0;
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        for (int val : priorities) {
+            priorityQueue.add(val);
+        }
+        for (int i = 0; i < priorities.length; i++) {
+            if (i == location) {
+                locationQueue.add(true);
+            } else {
+                locationQueue.add(false);
+            }
+        }
 
-		ArrayList<Integer> priority = new ArrayList<>();
-		for (int i = 0; i < length; i++) {
-			priority.add(priorities[i]);
-		}
-
-		ArrayList<String> wait = new ArrayList<>();
-		for (int i = 0; i < length; i++) {
-			String number = String.valueOf(i);
-			wait.add("A" + number);
-		}
-
-		String goal = wait.get(location);
-
-		while (true) {
-			Integer first = priority.get(0);
-			String str = wait.get(0);
-			for (int i = 0; i < priority.size(); i++) {
-				if (priority.get(i) > first) {
-					priority.remove(0);
-					priority.add(first);
-					wait.remove(0);
-					wait.add(str);
-					break;
-				}
-				if (i == priority.size() - 1) {
-					if (wait.get(0).equals(goal)) {
-						answer = count;
-						break;
-					}
-					priority.remove(0);
-					wait.remove(0);
-					count++;
-				}
-			}
-			if (wait.get(0).equals(goal)) {
-
-				return answer;
-			}
-		}
-	}
+        while (true) {
+            Integer priority = priorityQueue.poll();
+            boolean bool = true;
+            for (Integer val : priorityQueue) {
+                if (val > priority) { // 뒤에 우선순위가 더 큰 것이 있다면
+                    bool = false;
+                    break;
+                }
+            }
+            if (!bool) { // 뒤에 우선순위가 더 큰 것이 있다면
+                priorityQueue.add(priority);
+                Boolean bool2 = locationQueue.poll();
+                locationQueue.add(bool2);
+            } else {
+                if (locationQueue.poll() == true) {
+                    break;
+                }
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
