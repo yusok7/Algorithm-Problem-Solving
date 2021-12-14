7d5ee5b4db0b922597e019d2eb971f49cbf7a3ed
yusok7 <diddbtjr7@naver.com>
2021-12-14 17:48:27 +0900

    다이나믹 프로그래밍

--- src/baekjoon/silver/silver1/Baekjoon2156.java
+++ src/baekjoon/silver/silver1/Baekjoon2156.java
@@ -7,28 +7,30 @@ import java.util.Arrays;
 
 public class Baekjoon2156 {
 
-    public static int[] d;
+    public static int[] dp;
     public static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        d = new int[n+1];
-        arr = new int[n+1];
+
+        arr = new int[n + 1];
+        dp = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        d[1] = arr[1];
-        d[2] = arr[1] + arr[2];
-
+        dp[1] = arr[1];
+        if (n > 1) {
+            dp[2] = arr[1] + arr[2];
+        }
         for (int i = 3; i <= n; i++) {
-            d[i] = Math.max(arr[i] + d[i-2], arr[i] + arr[i-1] + d[i-3]);
+            dp[i] = Math.max(Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]), dp[i-1]);
         }
 
-        Arrays.sort(d);
-        System.out.println(d[n]);
+        Arrays.sort(dp);
+        System.out.println(dp[n]);
+
     }
 }
