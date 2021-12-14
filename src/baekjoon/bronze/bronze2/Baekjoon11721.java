d18fcecef3069e290df0d7388d1844ce7c947953
yusok7 <diddbtjr7@naver.com>
2021-12-14 11:29:07 +0900

    입출력 구현

---
+++ src/baekjoon/bronze/bronze2/Baekjoon11721.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze.bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon11721 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i));
+            if (i % 10 == 9) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
