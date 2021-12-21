ce86b12a6dcc9e0a238af6b91cf528dd2f4aaaf4
yusok7 <diddbtjr7@naver.com>
2021-12-21 11:27:10 +0900

    정렬 알고리즘

---
+++ src/baekjoon/silver/silver3/Baekjoon18310.java
@@ -0,0 +1,24 @@
+package baekjoon.silver.silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon18310 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[(n - 1) / 2]);
+    }
+}
