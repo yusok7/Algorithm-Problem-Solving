c5072ca82935a92cbc7da9ff09ae775712e0a5e7
yusok7 <diddbtjr7@naver.com>
2021-12-30 20:48:14 +0900

    BFS 알고리즘 (1차원)

--- src/baekjoon/silver/silver1/Baekjoon1697.java
+++ src/baekjoon/silver/silver1/Baekjoon1697.java
@@ -3,37 +3,75 @@ package baekjoon.silver.silver1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Baekjoon1697 {
 
-    public static Queue<Integer> queue = new LinkedList<>();
+    static class Node {
+        int index;
+        int count;
+
+        public Node(int index, int count) {
+            this.index = index;
+            this.count = count;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public static Queue<Node> queue = new LinkedList<>();
+    public static boolean[] boolArr = new boolean[100001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int count = 0;
+        int start = Integer.parseInt(st.nextToken());
+        int last = Integer.parseInt(st.nextToken());
 
-        queue.add(n);
-        while (true) {
-            List<Integer> list = new ArrayList<>();
-            for (Integer var : queue) {
-                if (var == k) {
-                    System.out.println(count);
-                    System.exit(0);
-                } else {
-                    list.add(var);
-                }
+        queue.add(new Node(start, 0));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int index = node.getIndex();
+            int count = node.getCount();
+            boolArr[index] = true;
+
+            if (index == last) {
+                System.out.println(count);
+                break;
             }
 
-            count++;
-
-            for (Integer var : list) {
-                queue.add(var-1);
-                queue.add(var+1);
-                queue.add(2*var);
+            for (int i = 0; i < 3; i++) {
+                int num = index;
+                if (i == 0) {
+                    num = num - 1;
+                    if (num < 0 || num > 100000 || boolArr[num]) continue;
+                    else {
+                        queue.add(new Node(num, count + 1));
+                    }
+                }
+                if (i == 1) {
+                    num = num + 1;
+                    if (num < 0 || num > 100000 || boolArr[num]) continue;
+                    else {
+                        queue.add(new Node(num, count + 1));
+                    }
+                }
+                if (i == 2) {
+                    num = 2 * num;
+                    if (num < 0 || num > 100000 || boolArr[num]) continue;
+                    else {
+                        queue.add(new Node(num, count + 1));
+                    }
+                }
             }
         }
     }
