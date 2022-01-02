f691017130684de63d401c30e4466ee8c11e6d54
yusok7 <diddbtjr7@naver.com>
2022-01-02 16:08:54 +0900

    정렬 알고리즘

---
+++ src/baekjoon/silver/silver5/Baekjoon15688.java
@@ -0,0 +1,26 @@
+package baekjoon.silver.silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon15688 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
