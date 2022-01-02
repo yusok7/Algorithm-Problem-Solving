fb524e0b056ac39ab047b85d6d6ec12a3df0cc8e
yusok7 <diddbtjr7@naver.com>
2022-01-02 15:58:37 +0900

    우선순위 큐 자료구조

--- src/baekjoon/silver/silver5/Baekjoon2751.java
+++ src/baekjoon/silver/silver5/Baekjoon2751.java
@@ -1,29 +1,27 @@
 package baekjoon.silver.silver5;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Baekjoon2751 {
 
-    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static PriorityQueue<Integer> queue = new PriorityQueue<>();
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(br.readLine());
-            pq.add(input);
+            int num = Integer.parseInt(br.readLine());
+            queue.add(num);
         }
 
         for (int i = 0; i < n; i++) {
-            bw.write(String.valueOf(pq.poll()));
-            bw.write("\n");
+            sb.append(queue.poll()).append("\n");
         }
-
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
 }
