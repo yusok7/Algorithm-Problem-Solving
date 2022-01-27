76793df01ceaba9ad26f755bc2e6ad2fc2d3e4c2
yusok7 <diddbtjr7@naver.com>
2022-01-27 11:33:16 +0900

    해시 자료구조

--- src/programmers/levelthree/BestAlbum.java
+++ src/programmers/levelthree/BestAlbum.java
@@ -5,12 +5,81 @@ import java.io.*;
 
 public class BestAlbum {
 
-    public static Map<String, Integer> map = new HashMap<>();
+    static class Song implements Comparable<Song> {
+        String genre;
+        int play;
+        int id;
+
+        public Song(String genre, int play, int id) {
+            this.genre = genre;
+            this.play = play;
+            this.id = id;
+        }
+
+        @Override
+        public int compareTo(Song o) {
+            if (this.genre.equals(o.genre)) {
+                return -(Integer.compare(this.play, o.play));
+            } else {
+                return this.genre.compareTo(o.genre);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        solution(genres, plays);
+    }
 
     public static int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        Map<String, ArrayList<Song>> map = new HashMap<>();
+        Map<String, Integer> count = new HashMap<>();
+        List<Song> list = new ArrayList<>();
+        for (String genre : genres) {
+            map.put(genre, new ArrayList<>());
+        }
+
         for (int i = 0; i < genres.length; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+            String genre = genres[i];
+            int play = plays[i];
+            int id = i;
+
+            Song song = new Song(genre, play, id);
+            ArrayList<Song> songs = map.get(genre);
+            songs.add(song);
+            Collections.sort(songs);
+            map.put(genre, songs);
+
+            count.put(genre, count.getOrDefault(genre, 0) + play);
+        }
+
+        for (String key : count.keySet()) {
+            Song song = new Song(key, count.get(key), 0);
+            list.add(song);
+        }
+        Collections.sort(list, (Song song1, Song song2) -> {
+            return -(song1.play - song2.play);
+        });
+
+        List<Integer> list2 = new ArrayList<>();
+        for (Song song : list) {
+            String genre = song.genre;
+            ArrayList<Song> songs = map.get(genre);
+            int index = 0;
+
+            for (int i = 0; i < songs.size(); i++) {
+                if (index == 2)
+                    break;
+                int id = songs.get(i).id;
+                list2.add(id);
+                index++;
+            }
+        }
+
+        int[] answer = new int[list2.size()];
+        for (int i = 0; i < list2.size(); i++) {
+            answer[i] = list2.get(i);
         }
 
         return answer;
