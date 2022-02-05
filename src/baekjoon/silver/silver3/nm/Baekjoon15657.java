611836bd8e00e1be13edecebc2ee1752daa15dcf
yusok7 <diddbtjr7@naver.com>
2022-02-05 10:54:03 +0900

    N과 M (8)

---
+++ src/baekjoon/silver/silver3/nm/Baekjoon15657.java
@@ -0,0 +1,49 @@
+package baekjoon.silver.silver3.nm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon15657 {
+
+    private static int n, m;
+    private static int[] arr;
+    private static int[] result;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        backtracking(0, 0);
+        System.out.println(sb.toString());
+
+    }
+
+    private static void backtracking(int start, int depth) {
+        if (depth == m) {
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            result[depth] = arr[i];
+            backtracking(i, depth + 1);
+        }
+    }
+}
