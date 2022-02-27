cb3a0e950b77eea981ba78b87667c50cf4dbe186
yusok7 <diddbtjr7@naver.com>
2022-02-27 11:27:47 +0900

    백트래킹 알고리즘 (n과 m)

---
+++ src/baekjoon/silver/silver3/nm/Baekjoon15665.java
@@ -0,0 +1,52 @@
+package baekjoon.silver.silver3.nm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Baekjoon15665 {
+
+    private static int n, m;
+    private static int[] arr;
+    private static int[] result;
+    private static Set<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        func(0);
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+
+    private static void func(int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int val : result) {
+                sb.append(val).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            result[depth] = arr[i];
+            func(depth + 1);
+        }
+    }
+}
