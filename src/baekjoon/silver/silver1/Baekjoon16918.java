2262640d4365b13574a9deffbee96ad73f06a006
yusok7 <diddbtjr7@naver.com>
2022-03-24 18:02:35 +0900

    구현 / 시뮬레이션

--- src/baekjoon/silver/silver1/Baekjoon16918.java
+++ src/baekjoon/silver/silver1/Baekjoon16918.java
@@ -3,120 +3,90 @@ package baekjoon.silver.silver1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon16918 {
 
-    static class Node {
-        int r;
-        int c;
-        int time;
+    // 북 동 남 서
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
 
-        public Node(int r, int c, int time) {
-            this.r = r;
-            this.c = c;
-            this.time = time;
-        }
-    }
-
-    private static Queue<Node> queue = new LinkedList<>();
-
-    private static String[][] arr;
-    private static Node[][] nodeArr;
-    private static boolean[][] bool;
-    private static int r, c, n;
-
-    // 동 서 남 북
-    private static int[] dx = {0, 0, 1, -1};
-    private static int[] dy = {1, -1, 0, 0};
+    private static String[][] answer;
+    private static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        arr = new String[r][c];
-        nodeArr = new Node[r][c];
-        bool = new boolean[r][c];
 
-        for (int i = 0; i < r; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < c; j++) {
-                arr[i][j] = String.valueOf(str.charAt(j));
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        answer = new String[n][m];
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                String str = String.valueOf(s.charAt(j));
+                if (str.equals("O"))
+                    arr[i][j] = 0;
+                else
+                    arr[i][j] = -1;
             }
         }
 
-        add(0);
-
-        for (int i = 2; i <= n; i++) {
-            // 폭탄 추가
-            if (i % 2 == 0) {
-                for (int j = 0; j < r; j++) {
-                    for (int k = 0; k < c; k++) {
-                        if (arr[j][k].equals(".")) {
-                            arr[j][k] = "O";
-                            Node node = new Node(j, k, i);
-                            queue.add(node);
-                            nodeArr[j][k] = node;
-                            bool[j][k] = true;
+        int time = 1;
+        while (true) {
+            if (time != 1) {
+                // 폭탄 설치 시간
+                if (time % 2 == 0) {
+                    for (int i = 0; i < arr.length; i++)
+                        for (int j = 0; j < arr[0].length; j++)
+                            if (arr[i][j] == -1)
+                                arr[i][j] = time;
+                } else {
+                    for (int i = 0; i < arr.length; i++) {
+                        for (int j = 0; j < arr[0].length; j++) {
+                            if (arr[i][j] == time - 3) {
+                                arr[i][j] = -1;
+                                for (int a = 0; a < 4; a++) {
+                                    int nx = i + dx[a];
+                                    int ny = j + dy[a];
+                                    if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+                                        continue;
+                                    if (arr[nx][ny] == time - 3)
+                                        continue;
+                                    arr[nx][ny] = -1;
+                                }
+                            }
                         }
-                    }
-                }
-            } else {
-                int size = queue.size();
-                for (int j = 0; j < size; j++) {
-                    Node peek = queue.peek();
-                    if (peek.time == i - 3) {
-                        Node poll = queue.poll();
-                        int row = poll.r;
-                        int col = poll.c;
-                        if (bool[row][col] == false)
-                            continue;
-                        arr[row][col] = ".";
-                        bool[row][col] = false;
-                        for (int k = 0; k < 4; k++) {
-                            int nx = row + dx[k];
-                            int ny = col + dy[k];
-                            if (nx < 0 || ny < 0 || nx >= r || ny >= c)
-                                continue;
-                            Node node = nodeArr[nx][ny];
-                            if (node.time == i - 3)
-                                continue;
-                            arr[nx][ny] = ".";
-                            bool[nx][ny] = false;
-                        }
-                    } else {
-                        break;
                     }
                 }
             }
+            if (time == k)
+                break;
+            time++;
         }
-        print();
-    }
+        func();
 
-    private static void print() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                sb.append(arr[i][j]);
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++){
+                sb.append(answer[i][j]);
             }
             sb.append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    private static void add(int time) {
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (arr[i][j].equals("O")) {
-                    Node node = new Node(i, j, time);
-                    queue.add(node);
-                    nodeArr[i][j] = node;
-                    bool[i][j] = true;
-                }
+    private static void func() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] != -1)
+                    answer[i][j] = "O";
+                else
+                    answer[i][j] = ".";
             }
         }
     }
